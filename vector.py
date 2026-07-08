d5306dea220527a2d72146cca7db67a74a31c61a
Chinmoy Majumder <chinmoymajumder269@gmail.com>
2026-07-08 23:05:20 +0530

    vector class

---
+++ vector.py
@@ -0,0 +1,65 @@
+import math
+
+class Vector:
+
+    epsilon = 1e-12
+
+    def __init__(self,components):
+        if not components:
+            raise ValueError("Vector can't be empty")
+        if not all(isinstance(x,(float,int)) for x in components):
+            raise TypeError("All vector components must be integers or floats")
+        
+        self.data = [float(x) for x in components]
+        self.dimension = len(self.data)
+
+    def __len__(self):
+        return self.dimension
+
+    def __getitem__(self,index):
+        return self.data[index]
+
+    def __repr__(self):
+        return f"Vector({self.data})"
+
+    def __add__(self,other):
+        if self.dimension != other.dimension:
+            raise ValueError("Vectors must be of equal dimensions for addition")
+        return Vector([a + b for a,b in zip(self.data,other.data)])
+
+    def __sub__(self,other):
+        if self.dimension != other.dimension:
+            raise ValueError("Vectors must be of equal dimensions for subtraction")
+        return Vector([a - b for a,b in zip(self.data,other.data)])
+    
+    def __mul__(self,other):
+        if isinstance(other,(int,float)):
+            return Vector([x*other for x in self.data])
+        return NotImplemented
+    
+    def __rmul__(self,other):
+        return self.__mul__(other)
+    
+    def __neg__(self):
+        return Vector([-x for x in self.data])
+
+    def dot(self,other):
+        if self.dimension != other.dimension:
+            raise ValueError("Vectors must be of equal dimension for dot product")
+        return sum(a*b for a,b in zip(self.data,other.data))
+    
+    def norm(self,n=2):
+        if n==2:
+            return math.sqrt(sum(x**2 for x in self.data))
+        elif n == 1:
+            return sum(abs(x) for x in self.data)
+        return (sum(abs(x)**n for x in self.data))**(1.0/n)
+    
+    def normalise(self,n=2):
+        current_norm = self.norm(n)
+        if current_norm < self.epsilon:
+            raise ValueError("Cannot normalize a zero-vector")
+        return Vector([x / current_norm for x in self.data])
+
+    def is_orthogonal_to(self,other):
+        return abs(self.dot(other)) < self.epsilon
